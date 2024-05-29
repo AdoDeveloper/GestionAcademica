22ffd111b0efb13a94fc259aca535aba4a2b387e
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2024-05-29 06:22:16 -0600

    Consumo de API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { EstudiantesService } from './services/estudiantes.service';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+selector: 'app-root',
+standalone: true,
+imports: [CommonModule],
+templateUrl: './app.component.html',
+styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GestionAcademica';
+title = 'GestionAcademica';
+// Haciendo inyeccion de dependencia
+private readonly estudiantesServices = inject(EstudiantesService);
+// Creando observable
+estudiantes$ = this.estudiantesServices.obtenerEstudiantes();
 }
